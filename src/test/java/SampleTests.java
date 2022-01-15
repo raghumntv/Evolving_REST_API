e121bcc2db6a1f5049a97dbafa203d541c8dcaaf
raghumn <raghumn@testvagrant.com>
2022-01-15 18:13:46 +0530

    initial commit

---
+++ src/test/java/SampleTests.java
@@ -0,0 +1,9 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTests {
+    @Test
+    public void shouldTestSomeThing(){
+        Assert.assertEquals(1,1);
+    }
+}
