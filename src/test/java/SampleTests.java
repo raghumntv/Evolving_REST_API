24ac9353f2593a6c03538769d835fbb26f129254
raghumn <raghumn@testvagrant.com>
2022-01-16 12:15:52 +0530

    adding create user api

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -1,4 +1,4 @@
-import org.testng.Assert;
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -13,4 +13,25 @@ public class SampleTests {
                     .statusCode(200)
                     .log().body();
     }
+
+    @Test
+    public void shouldCreateUser(){
+        given()
+                    .accept(ContentType.JSON)
+                    .contentType(ContentType.JSON)
+                    .header("Authorization","Bearer c7603baeda0fd898e3ab4f80f49c8cb7e920ee130ba06dbc37b577880128b5ce")
+                    .body("{\n" +
+                            "  \"name\": \"Tenali Ramakrishna\",\n" +
+                            "  \"gender\": \"male\",\n" +
+                            "  \"email\": \"tenali.ramakrishna1943@15ce.com\",\n" +
+                            "  \"status\": \"active\"\n" +
+                            "}")
+                .when()
+                    .post("https://gorest.co.in/public/v1/users")
+                .then()
+                    .log().body()
+                    .statusCode(201);
+
+    }
 }
+
